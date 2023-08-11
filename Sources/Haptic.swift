cb2f653b41dadb96ccb81ac7b3ef82fc4b22de94
Rens Wijnmalen <r.wijnmalen@yipyip.nl>
2023-08-11 17:11:27 +0200

    Added the intensity to the haptic impact types

--- Sources/Haptic.swift
+++ Sources/Haptic.swift
@@ -34,7 +34,7 @@ extension HapticFeedbackType {
 }
 
 public enum Haptic {
-    case impact(HapticFeedbackStyle)
+    case impact(HapticFeedbackStyle, CGFloat = 1.0)
     case notification(HapticFeedbackType)
     case selection
     
@@ -43,10 +43,20 @@ public enum Haptic {
         guard #available(iOS 10, *) else { return }
         
         switch self {
-        case .impact(let style):
+        case .impact(let style, let _intensity):
             let generator = UIImpactFeedbackGenerator(style: style.value)
             generator.prepare()
-            generator.impactOccurred()
+            if #available(iOS 13.0, *) {
+                var intensity = _intensity
+                if _intensity > 1 {
+                    intensity = 1
+                } else if _intensity < 0 {
+                    intensity = 0
+                }
+                generator.impactOccurred(intensity: intensity)
+            } else {
+                generator.impactOccurred()
+            }
         case .notification(let type):
             let generator = UINotificationFeedbackGenerator()
             generator.prepare()
